ca271116ff43a8cdab6533802b8e4159842fd4fc
ZapAnton <wc3ft2@gmail.com>
2018-04-06 00:31:48 +0300

    Initial commit

---
+++ black_red_tree.py
@@ -0,0 +1,13 @@
+class BlackRedTree:
+
+    def __init__(self):
+        print('Creating black-red tree!')
+
+        self.root = [1, 3]
+
+
+print('Hello world')
+
+tree = BlackRedTree()
+
+print(tree, 'Root =', tree.root)
