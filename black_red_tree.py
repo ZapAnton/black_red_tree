1160888820cdb56ce4aa4ef545536e2788533d56
ZapAnton <wc3ft2@gmail.com>
2018-04-11 20:29:49 +0300

    Added TreeNode subclass

--- black_red_tree.py
+++ black_red_tree.py
@@ -1,13 +1,23 @@
+from enum import Enum
+
+
 class BlackRedTree:
+
+    NODE_TYPE = Enum('NODE_TYPE', 'BLACK RED')
+
+    class TreeNode:
+
+        def __init__(self, node_type):
+            self.item = None
+
+            self.node_type = node_type
 
     def __init__(self):
         print('Creating black-red tree!')
 
-        self.root = [1, 3]
+        self.root = self.NODE_TYPE.BLACK
 
-
-print('Hello world')
 
 tree = BlackRedTree()
 
-print(tree, 'Root =', tree.root)
+print(tree.root)
