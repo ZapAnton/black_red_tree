75c062fabd6fc4bc694dc30a363da18111b5dd93
ZapAnton <wc3ft2@gmail.com>
2018-04-12 12:15:24 +0300

    Moved TreeNode from BlackRedTree class

--- black_red_tree.py
+++ black_red_tree.py
@@ -1,21 +1,27 @@
 from enum import Enum
 
 
+NODE_TYPE = Enum('NODE_TYPE', 'BLACK RED')
+
+
+class TreeNode:
+
+    def __init__(self, node_type):
+        self.item = None
+
+        self.node_type = node_type
+
+        self.left_child = TreeNode(NODE_TYPE.BLACK)
+
+        self.right_child = TreeNode(NODE_TYPE.BLACK)
+
+
 class BlackRedTree:
-
-    NODE_TYPE = Enum('NODE_TYPE', 'BLACK RED')
-
-    class TreeNode:
-
-        def __init__(self, node_type):
-            self.item = None
-
-            self.node_type = node_type
 
     def __init__(self):
         print('Creating black-red tree!')
 
-        self.root = BlackRedTree.TreeNode(BlackRedTree.NODE_TYPE.BLACK)
+        self.root = TreeNode(NODE_TYPE.BLACK)
 
 
 tree = BlackRedTree()
