8afc0626731069fa5ea781817e84194bf2ae18b4
ZapAnton <wc3ft2@gmail.com>
2018-04-28 11:09:20 +0300

    Added time measurement for finding min, max and random value

--- bench_tree.py
+++ bench_tree.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
 
     tree = BlackRedTree()
 
-    input_size = 5000000
+    input_size = 2000000
 
-    input_values = [random.randint(-1000, 1000) for _ in range(input_size + 1)]
+    input_values = random.sample(range(-10000000, 10000000), input_size)
 
     random.shuffle(input_values)
 
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
     end_memory = start_memory
 
-    start_time = timer()
+    start_time_insert = timer()
 
     for i, value in enumerate(input_values):
         if (i % 10000) == 0:
@@ -34,11 +34,38 @@ if __name__ == '__main__':
 
         tree.insert(value)
 
-    end_time = timer()
+    end_time_insert = timer()
+
+    start_time_min = timer()
+
+    tree.min()
+
+    end_time_min = timer()
+
+    start_time_max = timer()
+
+    tree.max()
+
+    end_time_max = timer()
+
+    start_time_find = timer()
+
+    tree.find(random.randint(-1000, 1000))
+
+    end_time_find = timer()
 
     print(
         'Time to insert {} values: {:.5f} seconds'
-        .format(input_size, end_time - start_time),
+        .format(input_size, end_time_insert - start_time_insert),
+
+        'Time to find min value in a tree: {:.8f} seconds'
+        .format(end_time_min - start_time_min),
+
+        'Time to find max value in a tree: {:.8f} seconds'
+        .format(end_time_max - start_time_max),
+
+        'Time to find random value in a tree: {:.8f} seconds'
+        .format(end_time_find - start_time_find),
 
         'Memory used to store {} values: {:.5f} MB'
         .format(input_size, end_memory - start_memory),
