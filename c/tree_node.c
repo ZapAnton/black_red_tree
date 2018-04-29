f88197fc4b53773ac9b725095b5cbd4d30050bbb
ZapAnton <wc3ft2@gmail.com>
2018-04-29 15:53:51 +0300

    Added new_node function

--- c/tree_node.c
+++ c/tree_node.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef enum{
+	BLACK,
+	RED,
+} NodeType;
 
 typedef struct TreeNode TreeNode;
-
 struct TreeNode {
 	int item;
+
+	NodeType node_type;
 
 	TreeNode* left_child;
 	
@@ -12,10 +19,44 @@ struct TreeNode {
 	TreeNode* parent;
 };
 
+TreeNode* new_node(int item) {
+	TreeNode* node = malloc(sizeof(TreeNode));
+
+	node->item = item;
+
+	node->node_type = RED;
+
+	node->right_child = NULL;
+
+	node->left_child = NULL;
+
+	return node;
+}
+
+
 int main(int argc, char** argv) {
-	puts("Hello from Node!");
+	TreeNode* node = new_node(5);
 
-	TreeNode node = {32};
+	TreeNode* parent = new_node(3);
 
-	printf("Tree node: %d Parent: %p\n", node.item, node.parent);
+	TreeNode* left_child = new_node(4);
+
+	node->parent = parent;
+
+	parent->right_child = node;
+
+	node->left_child = left_child;
+
+	left_child->parent = node;
+
+	printf("Node: %d, Parent: %d, Left Child: %d\n",
+			node->item, 
+			node->parent->item, 
+			node->left_child->item);
+
+	free(left_child);
+
+	free(node);
+
+	free(parent);
 }
